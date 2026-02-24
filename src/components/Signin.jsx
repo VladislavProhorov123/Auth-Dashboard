42f8286e0bfefa8271899a6cb9d4502fd34129c8
Vladislav <vladislavprokhorov1005@gmail.com>
2026-02-24 20:18:41 +0200

    Sign In

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const { session, signInUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    try {
+      const result = await signInUser(email, password);
+      if (result.success) {
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div>
+      <form onSubmit={handleSignIn} className="max-w-md m-auto pt-24">
+        <h2 className="font-bold pb-2">Sign In!</h2>
+        <p>
+          Don't have an account?{" "}
+          <Link className="text-blue-600 underline" to="/signup">
+            Sign up!
+          </Link>
+        </p>
+
+        <div className="flex flex-col gap-4 pt-4">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+            className="border p-2"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            type="password"
+            className="border p-2"
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className="bg-blue-600 text-white p-2 rounded"
+          >
+            Sign In
+          </button>
+
+          {error && <p className="text-red-500">{error}</p>}
+        </div>
+      </form>
+    </div>
+  );
 }
